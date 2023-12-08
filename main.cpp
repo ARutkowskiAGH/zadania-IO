57d555335c69c4a84a5beada3449279573b131d2
ARutkowskiAGH <147827294+ARutkowskiAGH@users.noreply.github.com>
2023-12-08 14:42:30 +0100

    Git na zajęciach

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Struktura.cpp"
+using namespace std;
+
+int main(){
+	cout<<"Witaj swiecie!\n";
+	cout<<"jest git!\n";
+	Struktura ja;
+	ja.imie = "Aleksander";
+	ja.nazwisko = "Rutkowski";
+	ja.grupa = 7;
+	return 0;
+}
